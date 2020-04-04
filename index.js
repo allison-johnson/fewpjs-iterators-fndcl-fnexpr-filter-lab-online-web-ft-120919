43ebf97dc4ffdef37de777b69d084ea2875f8047
allison_johnson <allison.cuttler@gmail.com>
2020-04-03 19:34:06 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(arr, str) {
+  return arr.filter(name => {
+    return name.toUpperCase() === str.toUpperCase()
+  });
+}//findMatching
+
+function fuzzyMatch(arr, str) {
+  return arr.filter(name => {
+    return name.toUpperCase().startsWith(str.toUpperCase())
+  });
+}//fuzzyMatch
+
+function matchName(arr, str) {
+  return arr.filter(driver => {
+    return driver.name.toUpperCase() === str.toUpperCase()
+  });
+}//matchName
